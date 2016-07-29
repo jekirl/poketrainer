465f6d6f66c718469d035bfa686f983d38056420
Jaime <thatguy707@outlook.com>
2016-07-29 03:30:37 -0500

    Added the ability to hide columns in the webtemplate. Add "IGNORE_COLUMNS":  ["Rating", "Test"], to your config file. This is an optional feature. Columns to be hidden should match column names.

--- web.py
+++ web.py
@@ -5,6 +5,8 @@ import csv
 from math import floor
 from collections import defaultdict
 import re
+import argparse
+
 from pgoapi.poke_utils import *
 from pgoapi.utilities import *
 import tempfile
@@ -18,6 +20,7 @@ pokemon_names = json.load(open("pokemon.en.json"))
 pokemon_details = {}
 pokemon_lvls = {}
 tcmpVals = []
+options = {}
 with open ("GAME_MASTER_POKEMON_v0_2.tsv") as tsv:
     reader = csv.DictReader(tsv, delimiter='\t')
     for row in reader:
@@ -47,6 +50,31 @@ with open ("PoGoPokeLvl.tsv") as tsv: #data gathered from here: https://www.redd
             "TCPMDif": float(row["TCPM Difference"])
         }
         tcmpVals.append(float(row["TotalCpMultiplier"]))
+
+def init_config():
+    parser = argparse.ArgumentParser()
+    config_file = "config.json"
+
+    # If config file exists, load variables from json
+    load = {}
+    if os.path.isfile(config_file):
+        with open(config_file) as data:
+            load.update(json.load(data))
+
+    # Read passed in Arguments
+    required = lambda x: not x in load['accounts'][0].keys()
+    parser.add_argument("-i", "--config_index", help="Index of account in config.json", default=0, type=int)
+    config = parser.parse_args()
+    load = load['accounts'][config.__dict__['config_index']]
+    # Passed in arguments shoud trump
+    for key,value in load.iteritems():
+        if key not in config.__dict__ or not config.__dict__[key]:
+            config.__dict__[key] = value
+
+    return config.__dict__
+config = init_config()
+
+
 
 def setMaxCP(pokemon, maxTCPM):
     if not all_in(['cp', 'cp_multiplier', 'individual_stamina', 'individual_attack', 'individual_defense'], pokemon):
@@ -94,11 +122,74 @@ def setMaxCP(pokemon, maxTCPM):
     pokeLvl = pokemon_lvls[pokemon['tcpm']]['PokemonLvl']
     pokemon['PowerUpResult'] = calcCP(pokemon, tcmpVals[pokeLvl], pokemon_details) - pokemon['cp']
 
+def setColumnsToIgnore(columnsToIgnore):
+    options['ignore_recent'] = ''
+    options['ignore_#'] = ''
+    options['ignore_name'] = ''
+    options['ignore_lvl'] = ''
+    options['ignore_rating'] = ''
+    options['ignore_IV'] = ''
+    options['ignore_CP'] = ''
+    options['ignore_max_CP'] = ''
+    options['ignore_candies'] = ''
+    options['ignore_candy_needed'] = ''
+    options['ignore_dust_needed'] = ''
+    options['ignore_power_up'] = ''
+    options['ignore_stamina'] = ''
+    options['ignore_attkIV'] = ''
+    options['ignore_staIV'] = ''
+    options['ignore_defIV'] = ''
+    options['ignore_move1'] = ''
+    options['ignore_move2'] = ''
+
+    for column in columnsToIgnore:
+        if column.lower() == 'recent':
+            options['ignore_recent'] = 'display: none;'
+        elif column.lower() == '#':
+            options['ignore_id'] = 'display: none;'
+        elif column.lower() == 'name':
+            options['ignore_name'] = 'display: none;'
+        elif column.lower() == 'lvl':
+            options['ignore_lvl'] = 'display: none;'
+        elif column.lower() == 'rating':
+            options['ignore_rating'] = 'display: none;'
+        elif column.lower() == 'iv':
+            options['ignore_IV'] = 'display: none;'
+        elif column.lower() == 'cp':
+            options['ignore_CP'] = 'display: none;'
+        elif column.lower() == 'max cp':
+            options['ignore_max_CP'] = 'display: none;'
+        elif column.lower() == 'candies':
+            options['ignore_candies'] = 'display: none;'
+        elif column.lower() == 'candy needed':
+            options['ignore_candy_needed'] = 'display: none;'
+        elif column.lower() == 'dust needed':
+            options['ignore_dust_needed'] = 'display: none;'
+        elif column.lower() == 'power up':
+            options['ignore_power_up'] = 'display: none;'
+        elif column.lower() == 'stamina':
+            options['ignore_stamina'] = 'display: none;'
+        elif column.lower() == 'att iv':
+            options['ignore_attkIV'] = 'display: none;'
+        elif column.lower() == 'sta iv':
+            options['ignore_staIV'] = 'display: none;'
+        elif column.lower() == 'def iv':
+            options['ignore_defIV'] = 'display: none;'
+        elif column.lower() == 'move 1':
+            options['ignore_move1'] = 'display: none;'
+        elif column.lower() == 'move 2':
+            options['ignore_move2'] = 'display: none;'
+
+
+
 @app.route("/<username>/pokemon")
 def inventory(username):
     c = get_api_rpc(username)
     if c is None:
         return("There is no bot running with the input username!")
+    options['DEFINE_POKEMON_LV'] = config.get("DEFINE_POKEMON_LV", "CP")
+    options['IGNORE_COLUMNS'] = config.get("IGNORE_COLUMNS", [])
+    setColumnsToIgnore(options['IGNORE_COLUMNS'])
     with open("data_dumps/%s.json"%username) as f:
         data = f.read()
         data = json.loads(data.encode())
@@ -121,6 +212,9 @@ def inventory(username):
                 pokemon['iv'] = pokemonIVPercentage(pokemon)
                 pokemon['acpm'] = calcACPM(pokemon, pokemon_details)
                 pokemon['tcpm'] = takeClosest((pokemon.get('cp_multiplier', 0) + pokemon['acpm']), tcmpVals) 
+                pokemon['lvl'] = pokemon_lvls[pokemon['tcpm']]['PokemonLvl']
+                #todo get rating
+                pokemon['rating'] = 0
                 pokemon['CalcCP'] = calcCP(pokemon, pokemon['tcpm'], pokemon_details)
                 pokemons.append(pokemon)
             if 'player_stats' in item:
@@ -137,8 +231,7 @@ def inventory(username):
         player['level_xp'] = player.get('experience',0)-player.get('prev_level_xp',0)
         player['hourly_exp'] = data.get("hourly_exp",0)
         player['goal_xp'] = player.get('next_level_xp',0)-player.get('prev_level_xp',0)
-        player['username'] = username
-        return render_template('pokemon.html', pokemons=pokemons, player=player, currency="{:,d}".format(currency), candy=candy, latlng=latlng, attacks=attacks)
+        return render_template('pokemon.html', pokemons=pokemons, player=player, currency="{:,d}".format(currency), candy=candy, latlng=latlng, attacks=attacks, options=options)
 
 
 def get_api_rpc(username):
