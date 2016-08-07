0db79d3c309b03e710e3f0032f240adf5658899b
rirze <cheedella.sourabh@gmail.com>
2016-08-07 13:53:50 -0400

    updated web.py

--- web.py
+++ web.py
@@ -10,8 +10,8 @@ from collections import defaultdict
 import zerorpc
 from flask import Flask, flash, jsonify, redirect, render_template, url_for
 
-from pgoapi.poke_lvl_data import TCPM_VALS
-from pgoapi.pokemon import Pokemon
+from poketrainer.poke_lvl_data import TCPM_VALS
+from poketrainer.pokemon import Pokemon
 
 
 class ReverseProxied(object):
