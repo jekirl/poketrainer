5bb8f07c8dca57a5a8d1577017ce235cbafe5857
xthedeus <fhov@itu.dk>
2016-08-09 15:12:30 +0200

    Socket url now works for proxies. Removed leading slash for images.

--- static/assets/js/modules/services/service.Socket.js
+++ static/assets/js/modules/services/service.Socket.js
@@ -2,8 +2,8 @@ angular.module('Poketrainer.Service.Socket', ['btford.socket-io'])
     .factory('PokeSocket', ['socketFactory', function (socketFactory) {
         return socketFactory({
             ioSocket: io.connect(
-                window.location.protocol + "//" + window.location.host + window.location.pathname.replace(/\/+$/g, '') + "/api",
-                {path: window.location.pathname.replace(/\/+$/g, '') + "/socket.io"}
+                window.location.protocol + '//' + window.location.host + '/api',
+                {path: window.location.pathname.replace(/\/+$/g, '') + '/socket.io'}
             )
         });
     }]);
