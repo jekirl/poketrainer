b7c1b5d2eb64fa06ce93559c1c58c8b7cc3fd7d9
xthedeus <fhov@itu.dk>
2016-08-09 10:34:49 +0200

    Fixes for socket url

--- web-ui/src/app/modules/services/service.Socket.js
+++ web-ui/src/app/modules/services/service.Socket.js
@@ -1,6 +1,9 @@
 angular.module('Poketrainer.Service.Socket', ['btford.socket-io'])
     .factory('PokeSocket', ['socketFactory', function (socketFactory) {
         return socketFactory({
-            ioSocket: io.connect(window.location.protocol + "//" + window.location.host + window.location.pathname + "/api")
+            ioSocket: io.connect(
+                window.location.protocol + "//" + window.location.host + window.location.pathname.replace(/\/+$/g, '') + "/api",
+                {path: window.location.pathname.replace(/\/+$/g, '') + "/socket.io"}
+            )
         });
     }]);
