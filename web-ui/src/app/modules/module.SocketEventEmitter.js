b4cfe3fc90622833c774eade4f828812c2454670
Lars Rasmussen <lars@1337.dk>
2016-08-10 22:13:21 +0200

    broadcasting push events from rootscope and down

--- web-ui/src/app/modules/module.SocketEventEmitter.js
+++ web-ui/src/app/modules/module.SocketEventEmitter.js
@@ -5,7 +5,9 @@ angular.module('Poketrainer.Module.SocketEventEmitter', ['Poketrainer.Service.So
         PokeSocket.forward(SocketEvent.UserStatus);
 
         PokeSocket.on(SocketEvent.Data, function getSocketPush(data){
-            console.log("Pushed: ", data);
+            console.log("Got push data: ", data);
+
+            $rootScope.$broadcast(data.event + ":" + data.action, data.data);
         });
 
         // Broadcast events using $rootScope.$broadcast([name], [data]);
